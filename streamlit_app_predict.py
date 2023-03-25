1b27ebe41644460349d33147b3d931da1f40ad3c
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-25 14:00:09 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -18,13 +18,14 @@ def load_image():
         path = os.path.abspath(name)
         print("abs path")
         print(path)
+	return path
         #print(Path.cwd())
 
 
 
-def predict(model):
-	#return model.predict(path).json()
-	return model.predict("https://kishuinvain1-streamlit-crack-streamlit-app-predict-9tmpw9.streamlit.app/~/+/media/edf9973037ae0e2650858998d807c405002cf5ca407d930e6116cda7.jpg", hosted=True).json()
+def predict(model, path):
+	return model.predict(path).json()
+	#return model.predict("https://kishuinvain1-streamlit-crack-streamlit-app-predict-9tmpw9.streamlit.app/~/+/media/edf9973037ae0e2650858998d807c405002cf5ca407d930e6116cda7.jpg", hosted=True).json()
 	
 	
 def main():
@@ -32,12 +33,12 @@ def main():
 	project = rf.workspace().project("fleet-crack-2-wg5qy")
 	model = project.version(1).model
 	st.title('Pretrained model demo')
-	image = load_image()
+	path = load_image()
 	result = st.button('Run on image')
 	if result:
 		st.write('Calculating results...')
 		#results = predict(model, "/home/kishore/Desktop/Verifygn_Tech/9_crack_detection_2/Fleet-Crack-2.v2i.folder/train/Crack/0000.rf.151752c1ef8868d48500e83601531186.jpg")
-		results = predict(model)
+		results = predict(model, path)
 		st.write(results["predictions"][0]["predictions"][0]["class"])
 		st.write(results["predictions"][0]["predictions"][0]["confidence"])
 		#print(results["predictions"][0]["predictions"][0]["class"])
