ba157286e0aadffeee987a120e13a0eafb816348
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-25 13:20:19 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -17,9 +17,9 @@ def load_image():
         
 
 
-def predict(model, path):
-	return model.predict(path).json()
-	
+def predict(model):
+	#return model.predict(path).json()
+	return model.predict("https://kishuinvain1-streamlit-crack-streamlit-app-predict-9tmpw9.streamlit.app/~/+/media/edf9973037ae0e2650858998d807c405002cf5ca407d930e6116cda7.jpg").jso()
 	
 	
 def main():
@@ -31,8 +31,8 @@ def main():
 	result = st.button('Run on image')
 	if result:
 		st.write('Calculating results...')
-		results = predict(model, "/home/kishore/Desktop/Verifygn_Tech/9_crack_detection_2/Fleet-Crack-2.v2i.folder/train/Crack/0000.rf.151752c1ef8868d48500e83601531186.jpg")
-		#results = predict(model)
+		#results = predict(model, "/home/kishore/Desktop/Verifygn_Tech/9_crack_detection_2/Fleet-Crack-2.v2i.folder/train/Crack/0000.rf.151752c1ef8868d48500e83601531186.jpg")
+		results = predict(model)
 		st.write(results["predictions"][0]["predictions"][0]["class"])
 		st.write(results["predictions"][0]["predictions"][0]["confidence"])
 		#print(results["predictions"][0]["predictions"][0]["class"])
