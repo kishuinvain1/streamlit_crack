1121655b0b4b5d70c43b0abf799c973d3509c0e7
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-28 12:10:49 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -10,6 +10,8 @@ import cv2
 
 
 def load_image():
+    path = None
+    f = None
     uploaded_file = st.file_uploader(label='Pick an image to test')
     print(uploaded_file)
     if uploaded_file is not None:
