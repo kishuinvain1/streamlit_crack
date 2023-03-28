4fdcb5a54792b913fb3a14e1c9ac53d1b06ee936
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-28 14:42:27 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -75,12 +75,13 @@ def main():
     
     st.title('Crack Detection Demo')
     image, svd_img = load_image()
-    #st.write('Enter the image URL')
-    #url = st.text_input('URL', '')
+    st.write('Enter the image URL')
+    url = st.text_input('URL', '')
     result = st.button('Predict')
     if result:
         st.write('Calculating results...')
-        results = predict(model2, svd_img.name)
+        #results = predict(model2, svd_img.name)
+        results = predict(model2, url)
         print(results)
         if len(results['predictions']) == 0:
             st.image(svd_img.name)
