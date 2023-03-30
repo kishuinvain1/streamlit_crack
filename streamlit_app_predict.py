42fc067744e0ff1b8ea96b6eef0cebd3f3dab916
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-03-30 14:44:08 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -18,9 +18,6 @@ def load_image():
     uploaded_file = st.file_uploader(label='Pick an image to test')
     print(uploaded_file)
     if uploaded_file is not None:
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-      
         
         image_data = uploaded_file.getvalue()
         
